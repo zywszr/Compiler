4dd3b68c9cf10aa3412918fbfc67bba5462d8b03
zywszr <assassain_lyc@sjtu.edu.cn>
2019-03-31 05:00:58 +0800

    added files

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+
+import FrontEnd.*;
+
+//import org.antlr.runtime.ANTLRInputStream;
+
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.*;
+//import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException, Exception {
+    //   InputStream in = new FileInputStream("test.txt");
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        MxStarLexer lexer = new MxStarLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MxStarParser parser = new MxStarParser(tokens);
+        parser.setErrorHandler(new BailErrorStrategy());
+
+        ParseTree tree = parser.program();
+/*
+        System.out.println("LISP:");
+        System.out.println(tree.toStringTree(parser));
+        System.out.println();
+*/
+
+        ASTBuilder buildAST = new ASTBuilder();
+
+        //System.out.println("what");
+        Node ASTroot = buildAST.visit(tree);
+
+        ScopeBuilder BuildScope = new ScopeBuilder();
+
+        BuildScope.programScopeBuild(ASTroot);
+        //System.out.println("what");
+        SemanticChecker checker = new SemanticChecker(BuildScope.rootScope);
+        try {
+            ASTroot.accept(checker);
+        } catch (SyntaxError error) {
+            System.out.println(error.toString() + " on Line: " + error.pos.line + ",  Column: " + error.pos.column);
+            throw error;
+        }
+    }
+}
